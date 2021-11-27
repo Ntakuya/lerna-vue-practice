6fdf19dfd811429b9853a419d04359c7c9624af6
Ntakuya <nagahama.takuya@outlook.jp>
2021-11-27 23:02:31 +0900

    feat(nuxt-v2) initial commit

--- packages/nuxt-v2/__tests__/nuxt-v2.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const nuxtV2 = require('..');
-
-describe('nuxt-v2', () => {
-    it('needs tests');
-});
--- packages/nuxt-v2/lib/nuxt-v2.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = nuxtV2;
-
-function nuxtV2() {
-    // TODO
-}
